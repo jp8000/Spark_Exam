123704830fd7ef3b042e16ccd09fd353d408df55
jp8000 <118046466+jp8000@users.noreply.github.com>
2023-05-27 19:09:50 +1000

    updated all data sets

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
+#Create a spark session 
 from pyspark.sql import SparkSession
 
+#Makes an object
 spark = SparkSession.builder.appName("main").getOrCreate()
 
 df_accounts_data = spark.read.option("header", "true").option("inferSchema","true")\
-    .csv("Spark_Exam/data/account_data.txt")
+    .csv("Spark_Exam/data/account_data.txt") # Creating a data frame using a csv file 
 
 df_customer_data = spark.read.option("header", "true").option("inferSchema","true")\
-    .csv("Spark_Exam/data/customer_data.txt")
+    .csv("Spark_Exam/data/customer_data.txt") # Creating a data frame using a csv file 
 
 
-df_customer_data.show()
+# Perform a join operation
+joined_df = df_accounts_data.join(df_customer_data, "customerId")
 
-df_accounts_data.show()
+# Show the joined DataFrame
+joined_df.show()
